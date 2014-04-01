25344b281d09e4a705635d21291b0ca8395a93d6
Kornel Lesiński <kornel.lesinski@ft.com>
2014-04-01 12:33:50 +0100

    Desperate parsing tricks to get inodes

--- diskspace.js
+++ diskspace.js
@@ -31,16 +31,62 @@ function check(drive, options, callback) {
 			});
 		});
 	} else {
-		exec("df -k '" + drive.replace(/'/g,"'\\''") + "'", function(error, stdout, stderr) {
+		exec("df -k " + (options.inodes ? '-i' : '') + "'" + drive.replace(/'/g,"'\\''") + "'", function(error, stdout, stderr) {
 			if (error) return callback(fail(stderr, error));
 
-			var lines = stdout.trim().split("\n");
-			var disk_info = lines[lines.length - 1].split(/[\s\n\r]+/);
+			// assume default shell tab is 8
+			var lines = stdout.replace(/\t/g,'        ').trimRight().split("\n");
+			if (lines.length < 2) {
+				return callback(fail("Unexpected output: " + stdout, -1));
+			}
 
-			callback(null, {
-				total: disk_info[1] * 1024,
-				free: disk_info[3] * 1024,
-			});
+			// implementations have varying number of columns and columns names/values with spaces
+			// also some use right-aligned colums - it's a mess
+			var headersLine = lines[0];
+			var dataLine = lines[lines.length-1];
+
+			// looks for column name and then for value underneath
+			// that overlaps either first or last character of the header name (for left or right alignment)
+			function getValue(headersLine, dataLine, columnName) {
+				var m = headersLine.match(new RegExp("^(.*?)\\b"+columnName+"\\b","i"));
+				if (!m) return undefined;
+
+				var start = m[1].length;
+				var end = m[0].length;
+
+				while(start > 0 && dataLine.charAt(start) != ' ') {
+					start--;
+				}
+				while(end < dataLine.length-1 && dataLine.charAt(end) != ' ') {
+					end++;
+				}
+
+				var value = parseInt(dataLine.substring(start, end), 10);
+				if (isNaN(value)) return undefined;
+				return value;
+			}
+
+			var results = {
+				total: getValue(headersLine, dataLine, "1(?:024|K)?-?blocks"),
+				used: getValue(headersLine, dataLine, "used"),
+				free: getValue(headersLine, dataLine, "avail(?:able)?"),
+				ifree: getValue(headersLine, dataLine, "ifree"),
+				iused: getValue(headersLine, dataLine, "iused"),
+			};
+
+			if (results.total === undefined && results.used !== undefined && results.free !== undefined ) {
+				results.total = results.used + results.free;
+			}
+
+			if (results.total) results.total *= 1024;
+			if (results.used) results.used *= 1024;
+			if (results.free) results.free *= 1024;
+
+			if (results.ifree !== undefined && results.iused !== undefined ) {
+				results.itotal = results.ifree + results.iused;
+			}
+
+			callback(null, results);
 		});
 	}
 }
--- test.js
+++ test.js
@@ -1,23 +1,42 @@
+'use strict';
 var diskspace = require('./diskspace.js'); //use just diskspace if installed via NPM
-var os = require('os');
-var drive_letter = null;
+var assert = require('assert');
 
-if (os.type() == 'Windows_NT')
-{
-	drive_letter = 'C';
-}
-else if (os.type() == 'Darwin' || os.type() == 'Linux') //Mac OS or Linux
-{
-	drive_letter = '/';
-}
-else
-{
-	drive_letter = null;
-}
-
-diskspace.check(drive_letter, function (total, free, status)
-{
-	console.log('Total: ' + total);
-	console.log('Free: ' + free);
-	console.log('Status: ' + status);
+diskspace.check('/', {inodes:true, exec:function(cmd,cb){
+	cb(null,
+	"Filesystem     1k-blocks      Used Available Capacity  iused    ifree %iused  Mounted on\n"+
+	"/dev/disk1     234643592 137112448  97019144    59% 17203054 12127393   59%   /\n"
+	);
+}}, function(err, res){
+	assert.equal(res.used, 137112448*1024);
+	assert.equal(res.free, 97019144*1024);
+	assert.equal(res.total, 234643592*1024);
+	assert.equal(res.iused, 17203054);
+	assert.equal(res.ifree, 12127393);
 });
+
+diskspace.check('/', {inodes:true, exec:function(cmd,cb){
+	cb(null,
+	"Filesystem            Inodes   IUsed   IFree IUse% Mounted on\n"+
+	"/dev/vdb             16777216 2679222 14097994   16% /slowdisk\n"
+	);
+}}, function(err, res){
+	assert.equal(res.iused, 2679222);
+	assert.equal(res.ifree, 14097994);
+	assert.equal(res.itotal, 14097994+2679222);
+	assert.ok(res.used === undefined);
+});
+
+diskspace.check('/', {inodes:true, exec:function(cmd,cb){
+	cb(null,
+	"Filesystem            Inodes   IUsed   IFree IUse% Mounted on\n"+
+	"/dev/vdc             7077888 2662227 4415661   38% /home\n"+
+	"dev02-shell01.example.com:/slowdisk/foo bar\n"+
+	"                     16777216 2679222 14097994   16% /opt/foo bar\n"
+	);
+}}, function(err, res){
+	assert.equal(res.iused, 2679222);
+	assert.equal(res.ifree, 14097994);
+	assert.ok(res.total === undefined);
+});
+
